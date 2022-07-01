8d191c221cbd3fbb403290308adff793ec2a68de
guangyaw <guangyaw@hotmail.com>
2022-07-01 09:12:28 +0800

    remove commit

--- code/model.py
+++ code/model.py
@@ -24,20 +24,6 @@ class FileInformation(BaseModel):
     file_type: str
 
 
-# userテーブルのモデルUserTableを定義
-# class UserTable(Base):
-#     __tablename__ = 'user'
-#     id = Column(Integer, primary_key=True, autoincrement=True)
-#     name = Column(String(30), nullable=False)
-#     age = Column(Integer)
-#
-#
-# class User(BaseModel):
-#     id: int
-#     name: str
-#     age: int
-
-
 def main():
     Base.metadata.create_all(bind=ENGINE)
 
